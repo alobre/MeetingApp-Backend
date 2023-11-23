d1212e8e24f8195d273f45f970f783dc0bd47137
ana-m-v <ana.is.learning@gmail.com>
2023-11-23 10:35:41 +0100

    seed.js updated with tables/demo data

--- seed.js
+++ seed.js
@@ -1,13 +1,14 @@
-const { Pool } = require('pg');
-
+const { Pool } = require("pg");
 
 // Import required packages and configure the pool (similar to index.js)
 const pool = new Pool({
-    connectionString: 'postgres://pddeltbh:C_DtEUKeLuMCHmeEQE01fBMNNyhIR-W0@cornelius.db.elephantsql.com/pddeltbh',
-  });
-  
-  // Create tables if it doesn't exist
-  pool.query(`
+  connectionString:
+    "postgres://pddeltbh:C_DtEUKeLuMCHmeEQE01fBMNNyhIR-W0@cornelius.db.elephantsql.com/pddeltbh",
+});
+
+// Create tables if it doesn't exist
+pool.query(
+  `
   DROP TABLE IF EXISTS action_point_comments CASCADE;
   DROP TABLE IF EXISTS todo CASCADE;
   DROP TABLE IF EXISTS action_point_subpoints CASCADE;
@@ -33,11 +34,20 @@ const pool = new Pool({
     is_finalized boolean NOT NULL
 );
 
+
+CREATE TABLE IF NOT EXISTS meeting_series (
+  meeting_series_id serial PRIMARY KEY,
+  meeting_series_name character varying(255) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS meetings (
   meeting_id serial PRIMARY KEY,
+  meeting_series_id integer REFERENCES meeting_series(meeting_series_id),
   agenda_id integer REFERENCES agendas(agenda_id),
+  user_id integer REFERENCES users(user_id),
   title character varying(255) NOT NULL,
   address character varying(255) NOT NULL,
+  building character varying(255),
   room character varying(255) NOT NULL,
   date date NOT NULL,
   "start_time" time without time zone NOT NULL,
@@ -53,19 +63,14 @@ CREATE TABLE IF NOT EXISTS notifications (
 );
 
 
-
-CREATE TABLE IF NOT EXISTS meeting_series (
-    meeting_series_id serial PRIMARY KEY,
-	  meeting_id integer REFERENCES meetings(meeting_id),
-    meeting_series_name character varying(255) NOT NULL,
-    user_id integer REFERENCES users(user_id)
-);
-
 CREATE TABLE IF NOT EXISTS meeting_members (
-    user_id serial PRIMARY KEY,
-    meeting_id integer REFERENCES meetings(meeting_id),
-    edit_agenda boolean NOT NULL,
-    is_owner boolean NOT NULL
+  user_id integer,
+  meeting_id integer,
+  edit_agenda boolean NOT NULL,
+  is_owner boolean NOT NULL,
+  PRIMARY KEY (user_id, meeting_id),
+  FOREIGN KEY (meeting_id) REFERENCES meetings(meeting_id),
+  FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 
@@ -95,34 +100,88 @@ CREATE TABLE IF NOT EXISTS action_point_comments (
     action_point_id integer NOT NULL
 );
 
-  `, (err, result) => {
+  `,
+  (err, result) => {
     if (err) {
-      console.error('Error creating the tables', err);
+      console.error("Error creating the tables", err);
     } else {
-      console.log('tables created successfully');
+      console.log("tables created successfully");
     }
-  });
+  }
+);
+
+// Seed the table with initial data
+pool.query(
+  `
+  INSERT INTO meeting_series (meeting_series_name)
+  VALUES ('Weekly Team Meetings');
   
-  // Seed the table with initial data
-  pool.query(`
+  INSERT INTO meeting_series (meeting_series_name)
+  VALUES ('Project Meetings');
+  
+  INSERT INTO users (user_id, ldap_name, first_name, last_name, email, password)
+  VALUES (1,'john_doe', 'John', 'Doe', 'john@example.com', 'password123');
+  
+  INSERT INTO users (user_id, ldap_name, first_name, last_name, email, password)
+  VALUES (2, 'jane_smith', 'Jane', 'Smith', 'jane@example.com', 'pass123word');
+  
+  INSERT INTO users (user_id, ldap_name, first_name, last_name, email, password)
+  VALUES (3, 'bob_johnson', 'Bob', 'Johnson', 'bob@example.com', 'securepass321');
   
   INSERT INTO agendas (is_finalized)
-  VALUES
-    (true),
-    (false);
+    VALUES
+      (true),
+      (false);
   
-  INSERT INTO meetings (agenda_id, title, address, room, date, start_time, end_time)
-  VALUES (1, 'Team Meeting', '123 Main Street', 'Office Building A', '2023-11-15', '09:00:00', '10:30:00');
-
-  INSERT INTO meetings (agenda_id, title, address, room, date, start_time, end_time)
-  VALUES (2, 'Project Review', '456 Elm Avenue', 'Conference Center B', '2023-11-20', '14:00:00', '16:00:00');
+      INSERT INTO meetings (meeting_series_id, agenda_id, user_id, title, address, building, room, date, start_time, end_time)
+      VALUES (7, 7, 1, 'Team Meeting 1', '123 Main St', 'Office A', 'Conference Room A', '2023-11-24', '09:00', '10:30');
+      
+      INSERT INTO meetings (meeting_series_id, agenda_id, user_id, title, address, building, room, date, start_time, end_time)
+      VALUES (7, 7, 1, 'Team Meeting 2', '123 Main St', 'Office A', 'Conference Room A', '2023-12-01', '09:30', '11:00');
+      
+      INSERT INTO meetings (meeting_series_id, agenda_id, user_id, title, address, building, room, date, start_time, end_time)
+      VALUES (7, 7, 1, 'Team Meeting 3', '123 Main St', 'Office A', 'Conference Room B', '2023-12-08', '10:00', '11:30');
+      
+      -- Meeting series 2
+      INSERT INTO meetings (meeting_series_id, agenda_id, user_id, title, address, building, room, date, start_time, end_time)
+      VALUES (8, 8, 2, 'Project Finance', '456 Elm St', 'Office B', 'Meeting Room 1', '2023-11-30', '14:00', '16:00');
+      
+      INSERT INTO meetings (meeting_series_id, agenda_id, user_id, title, address, building, room, date, start_time, end_time)
+      VALUES (8, 8, 2, 'Project Review', '456 Elm St', 'Office B', 'Meeting Room 2', '2023-12-07', '14:30', '16:30');
   
+      INSERT INTO action_points (agenda_id, text)
+VALUES (7, 'Opening discussion'),
+       (7, 'Finance report review');
 
-  `, (err, result) => {
+       INSERT INTO action_points (agenda_id, text)
+VALUES (8, 'Opening discussion'),
+       (8, 'Finance report review');
+
+-- Inserting action point subpoints
+INSERT INTO action_point_subpoints (action_point_id, message)
+VALUES (7, 'Discuss upcoming projects'),
+       (8, 'Analyze quarterly profits'),
+       (8, 'Budget allocation review');
+
+-- Inserting todo items related to action point subpoints
+INSERT INTO todo (action_point_subpoint_id, user_id)
+VALUES (1, 1),
+       (3, 2);
+
+-- Inserting action point comments
+INSERT INTO action_point_comments (user_id, comment_text, action_point_id)
+VALUES (1, 'Suggest discussing marketing strategies during the opening', 5),
+       (2, 'Would like to explore investment options for the finance report', 6);
+
+
+
+
+  `,
+  (err, result) => {
     if (err) {
-      console.error('Error seeding the users table', err);
+      console.error("Error seeding the users table", err);
     } else {
-      console.log('Data seeded successfully');
+      console.log("Data seeded successfully");
     }
-  });
-  
+  }
+);
